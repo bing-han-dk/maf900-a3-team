af04968bb3e3a65532a349c62dded14784bbd6be
bing-han-dk <s224699586@deakin.edu.au>
2025-10-12 21:51:39 +1100

    realize functions of two parameter regression and summary statistics for table 3

--- 02_build/stage_functions.R
+++ 02_build/stage_functions.R
@@ -3,6 +3,7 @@ library(dplyr)
 
 # --- functions start ----
 
+
 # beta regression
 estimate_beta <- function(data, min_obs = 1) {
   if (nrow(data) < min_obs) {
@@ -19,6 +20,7 @@ estimate_beta <- function(data, min_obs = 1) {
                       ,sd_ret=sd_ret,sd_resid=sd_resid))
   }
 }
+
 
 # portfolio formation function
 assign_portfolios <- function(beta_df, n_port = 20) {
@@ -44,5 +46,45 @@ assign_portfolios <- function(beta_df, n_port = 20) {
   beta_df$portfolio <- portfolio_vec
   return(beta_df)
 }
+
+
+# two parameter regression function 
+run_fmb <- function(data, formula_str) {
+  data %>%
+    group_by(year, month) %>%
+    group_modify(~ {
+      model <- lm(as.formula(formula_str), data = .x)
+      tidy_res <- broom::tidy(model)
+      tidy_res$r_squared <- summary(model)$r.squared
+      tidy_res
+    }) %>%
+    ungroup()
+}
+
+
+# summary statistics (mean, t-stat, autocorrelation, R²) 
+# for Fama-MacBeth regression coefficients across time.
+fmb_coef_stats <- function(fmb_model, rf_data) {
+  fmb_model %>%
+    left_join(rf_data %>% select(year, month, rf), by = c("year", "month")) %>%
+    group_by(term) %>%
+    summarise(
+      mean_gamma = mean(estimate, na.rm = TRUE),
+      sd_gamma = sd(estimate, na.rm = TRUE),
+      t_stat = mean_gamma / (sd_gamma / sqrt(n())),
+      
+      acf1 = acf(estimate, plot = FALSE)$acf[2],  # ρ₀(γ)
+      
+      mean_gamma_rf = mean(estimate - rf, na.rm = TRUE),
+      t_gamma_rf = mean_gamma_rf / (sd_gamma / sqrt(n())),
+      acf1_gamma_rf = acf(estimate - rf, plot = FALSE)$acf[2],
+      
+      mean_r2 = mean(r_squared, na.rm = TRUE),
+      sd_r2 = sd(r_squared, na.rm = TRUE),
+      
+      .groups = "drop"
+    )
+}
+
 
 # --- functions end ---
